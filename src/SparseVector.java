a94ffc1af1249162d5c7b91d3118dbfcbce2c9ee
jimburgia <jimburgi@purdue.edu>
2014-11-20 22:16:07 -0500

    Finished the listing of the movies after performing K nearest neighbors algorithm

--- src/SparseVector.java
+++ src/SparseVector.java
@@ -1,51 +1,56 @@
-import java.util.*;
-import java.lang.*;
-public class SparseVector{
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SparseVector {
   public List<IntPair> data;
-  public SparseVector(){
+
+  public SparseVector() {
     data = new ArrayList<IntPair>();
   }
-  
-  //Appends a rating to the vector.
-  public void addRating(int index, double value){
+
+  // Appends a rating to the vector.
+  public void addRating(int index, double value) {
     data.add(new IntPair(index, value));
   }
-  
-  //If for some reason the ratings are not added in order of index, call this function.
-  public void sortByIndex(){
+
+  // If for some reason the ratings are not added in order of index, call this function.
+  public void sortByIndex() {
     Collections.sort(data);
   }
-  
-  //Gets the rating at a given index. -1 indicates no rating.
-  public double getRating(int index){
-    for (int i=0; i<data.size(); i++){
-      if (data.get(i).index == index){
+
+  // Gets the rating at a given index. -1 indicates no rating.
+  public double getRating(int index) {
+    for (int i = 0; i < data.size(); i++) {
+      if (data.get(i).index == index) {
         return data.get(i).value;
       }
     }
     return -1;
   }
-  
-  public double distanceFrom(SparseVector sv){
-    double assumedDiff = 1.5; //The assumed difference in rating if we don't have two ratings for a movie.
+
+  public double distanceFrom(SparseVector sv) {
+    double assumedDiff = 1.5; // The assumed difference in rating if we don't have two ratings for a
+                              // movie.
     this.sortByIndex();
     sv.sortByIndex();
     double runningSum = 0;
     double diffsCounted = 0;
-    for (int i=0; i<data.size(); i++){
+    for (int i = 0; i < data.size(); i++) {
       double otherRating = sv.getRating(data.get(i).index);
-      if (otherRating < 0) continue;
+      if (otherRating < 0)
+        continue;
       double diff = (data.get(i).value - otherRating);
       diffsCounted++;
       runningSum += diff * diff;
     }
-    double diffsNotCounted = MatrixBuilder.movies.Movies.size()-diffsCounted;
-    runningSum += assumedDiff*assumedDiff*diffsNotCounted;
+    double diffsNotCounted = MovieList.Movies.size() - diffsCounted;
+    runningSum += assumedDiff * assumedDiff * diffsNotCounted;
     return Math.sqrt(runningSum);
   }
-  
-  //For debugging.
-  public static void main(String[] args){
+
+  // For debugging.
+  public static void main(String[] args) {
     SparseVector sv = new SparseVector();
     sv.addRating(23, 4);
     sv.addRating(30, 3.5);
@@ -56,10 +61,10 @@ public class SparseVector{
     sv2.addRating(52, 4);
     System.out.println(sv.distanceFrom(sv2));
   }
-  
-  //For debugging. Prints the contents of the vector.
-  private void printContents(){
-    for (int i=0; i<data.size(); i++){
+
+  // For debugging. Prints the contents of the vector.
+  private void printContents() {
+    for (int i = 0; i < data.size(); i++) {
       System.out.println(data.get(i).index + "\t" + data.get(i).value);
     }
   }
